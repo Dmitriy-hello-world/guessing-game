ddf79f0aec7a8def4aae0e54d24cf14f6160d95b
Dmitriy <easportstng@gmail.com>
2021-08-22 16:36:32 +0300

    init: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.first = 0;
+        this.last = 0;
+        this.middle = 0;
+    }
 
     setRange(min, max) {
-
+        this.first = min;
+        this.last = max;
     }
 
     guess() {
-
+        this.middle = Math.round((this.first + this.last)/2);
+        return this.middle;
     }
 
     lower() {
-
+        this.last = this.middle;
     }
 
     greater() {
-
+        this.first = this.middle;
     }
 }
 
